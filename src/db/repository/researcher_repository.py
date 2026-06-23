be2049da93ae9d1ba836c93c7dca47588c9115cd
joaofig <joao.figueira@gmail.com>
2026-06-23 20:01:02 +0100

    Refactor `ResearcherRepository`: Migrate methods to class-based structure, replace direct connections with `get_connection`, and improve readability.

--- src/db/repository/researcher_repository.py
+++ src/db/repository/researcher_repository.py
@@ -1,80 +1,83 @@
 import asyncio
-from sqlite3 import Connection
+from typing import List
 
+from db import get_connection
 from src.db import SQLCache
 from src.models import Researcher
 
 
-def get_all(conn: Connection) -> list[Researcher]:
-    conn.row_factory = lambda _, row: Researcher(
-        id=row[0],
-        number=row[1],
-        name=row[2],
-        comments=row[3],
-    )
-    cache = SQLCache()
-    sql = cache.get("researcher/get_all.sql")
-    return conn.execute(sql).fetchall()
-
-
-def get(conn: Connection, researcher_id: int) -> Researcher | None:
-    conn.row_factory = lambda _, row: Researcher(
-        id=row[0],
-        number=row[1],
-        name=row[2],
-        comments=row[3],
-    )
-    cache = SQLCache()
-    sql = cache.get("researcher/get.sql")
-    return conn.execute(sql, (researcher_id,)).fetchone()
-
-
-def get_by_number(conn: Connection, researcher_number: str) -> Researcher | None:
-    conn.row_factory = lambda _, row: Researcher(
-        id=row[0],
-        number=row[1],
-        name=row[2],
-        comments=row[3],
-    )
-    cache = SQLCache()
-    sql = cache.get("researcher/get_by_number.sql")
-    return conn.execute(sql, (researcher_number,)).fetchone()
-
-
-def save(conn: Connection, researcher: dict) -> dict:
-    cache = SQLCache()
-    if researcher["id"] is None:
-        sql = cache.get("researcher/save.sql")
-        cur = conn.execute(
-            sql,
-            (researcher["number"], researcher["name"], researcher["comments"])
-        )
-        researcher["id"] = cur.lastrowid
-        cur.close()
-    else:
-        conn.execute(cache.get("researcher/update.sql"),
-                     (researcher["number"], researcher["name"], researcher["comments"], researcher["id"]))
-    conn.commit()
-    return researcher
-
-
-def delete(conn: Connection, researcher_id: int) -> None:
-    cache = SQLCache()
-    conn.execute(cache.get("researcher/delete.sql"), (researcher_id,))
-
-
 class ResearcherRepository:
-    def __init__(self, conn: Connection) -> None:
-        self.conn: Connection = conn
+    def __init__(self):
+        self.cache = SQLCache()
 
-    async def list(self) -> list[Researcher]:
-        return await asyncio.to_thread(get_all, self.conn)
+    def get_all(self) -> list[Researcher]:
+        conn = get_connection()
+        conn.row_factory = lambda _, row: Researcher(
+            id=row[0],
+            number=row[1],
+            name=row[2],
+            comments=row[3],
+        )
+        sql = self.cache.get("researcher/get_all.sql")
+        return conn.execute(sql).fetchall()
+
+    def get_by_id(self, researcher_id: int) -> Researcher | None:
+        conn = get_connection()
+        conn.row_factory = lambda _, row: Researcher(
+            id=row[0],
+            number=row[1],
+            name=row[2],
+            comments=row[3],
+        )
+        sql = self.cache.get("researcher/get.sql")
+        return conn.execute(sql, (researcher_id,)).fetchone()
+
+    def get_by_number(self, researcher_number: str) -> Researcher | None:
+        conn = get_connection()
+        conn.row_factory = lambda _, row: Researcher(
+            id=row[0],
+            number=row[1],
+            name=row[2],
+            comments=row[3],
+        )
+        sql = self.cache.get("researcher/get_by_number.sql")
+        return conn.execute(sql, (researcher_number,)).fetchone()
+
+    def _save(self, researcher: dict) -> dict:
+        conn = get_connection()
+        if researcher["id"] is None:
+            sql = self.cache.get("researcher/save.sql")
+            cur = conn.execute(
+                sql,
+                (researcher["number"], researcher["name"], researcher["comments"])
+            )
+            researcher["id"] = cur.lastrowid
+            cur.close()
+        else:
+            conn.execute(self.cache.get("researcher/update.sql"),
+                         (researcher["number"], researcher["name"], researcher["comments"], researcher["id"]))
+        conn.commit()
+        return researcher
+
+    def _delete(self, researcher_id: int) -> None:
+        conn = get_connection()
+        conn.execute(self.cache.get("researcher/delete.sql"), (researcher_id,))
+
+    async def list(self) -> List[Researcher]:
+        return await asyncio.to_thread(self.get_all)
 
     async def get(self, researcher_id: int) -> Researcher | None:
-        return await asyncio.to_thread(get, self.conn, researcher_id)
+        return await asyncio.to_thread(self.get_by_id, researcher_id)
 
     async def save(self, researcher: Researcher) -> None:
-        await asyncio.to_thread(save, self.conn, researcher)
+        await asyncio.to_thread(self._save, researcher.to_dict())
 
     async def delete(self, researcher_id: int) -> None:
-        await asyncio.to_thread(delete, self.conn, researcher_id)
+        await asyncio.to_thread(self._delete, researcher_id)
+
+
+
+class StudyResearcherRepository:
+    def __init__(self):
+        self.cache = SQLCache()
+
