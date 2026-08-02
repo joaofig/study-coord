0e082620f49f7aa68d92b78e4b86a05f946a8aba
joaofig <joao.figueira@gmail.com>
2026-08-02 22:01:24 +0100

    Debug DSN

--- app/src/repositories/postgres/client.py
+++ app/src/repositories/postgres/client.py
@@ -31,4 +31,5 @@ async def get_postgres_client() -> AsyncConnection:
         
         dsn = f"postgresql://{user}:{password}@{host}:{port}/{db}"
 
+    print(dsn)
     return await psycopg.AsyncConnection.connect(dsn)
