9475661eb9d19fdc5fe88452dc5a6da39ac0d38d
joafigu <joao.figueira@daimlertruck.com>
2026-08-10 16:38:37 +0100

    Refactor database schema: consolidate table definitions into `create_tables.sql`, introduce `drop_tables.sql` for cleanup, and update ownership management with `sevalla_user.sql`.

---
+++ db/sevalla/drop_tables.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS public.adverse_event;
+DROP TABLE IF EXISTS public.monitoring;
+DROP TABLE IF EXISTS public.protocol;
+DROP TABLE IF EXISTS public.study_researcher;
+DROP TABLE IF EXISTS public.visit;
+DROP TABLE IF EXISTS public.patient;
+DROP TABLE IF EXISTS public.researcher;
+DROP TABLE IF EXISTS public.study;
+
+DROP TABLE IF EXISTS public."user";
---
+++ db/sevalla/sevalla_user.sql
@@ -0,0 +1,27 @@
+
+ALTER TABLE IF EXISTS public.adverse_event
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.monitoring
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.patient
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.protocol
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.researcher
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.study
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.study_researcher
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public."user"
+    OWNER to "creepy-aquamarine-tortoise";
+
+ALTER TABLE IF EXISTS public.visit
+    OWNER to "creepy-aquamarine-tortoise";
