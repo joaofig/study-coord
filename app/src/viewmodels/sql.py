2c2c11825aeb4dcc69072bea1e40515e28bb29fc
joafigu <joao.figueira@daimlertruck.com>
2026-08-17 12:10:13 +0100

    Refactor: enhance `SQLView` layout, extract `SQLGrid` to a separate component, rename methods for clarity, and streamline query execution flow.

--- app/src/viewmodels/sql.py
+++ app/src/viewmodels/sql.py
@@ -1,32 +1,55 @@
-from typing import Any, LiteralString, cast
+from typing import Any, LiteralString, cast, Tuple
 
-from psycopg import AsyncConnection
+from nicegui import binding
+from psycopg import AsyncConnection, Column
+from psycopg.errors import Error
 
 from src.repositories.postgres.base import PostgresCentral
 from src.tools.observability import GridList
 from src.viewmodels import ViewModel
 
 
-async def run_query(query: str) -> list[dict[str, Any]]:
+async def run_query(query: str) -> Tuple[list[dict[str, Any]], list[Column] | None]:
     """Executes a database query and returns fetched records asynchronously"""
     conn = await PostgresCentral().connect()
     if conn:
         async with conn.cursor() as cur:
             await cur.execute(cast(LiteralString, query))
-            return await cur.fetchall()
-    return []
+            return await cur.fetchall(), cur.description
+    return [], []
 
 
+@binding.bindable_dataclass
 class SQLViewModel(ViewModel):
-    def __init__(self):
+    query: str = ""
+    result = GridList()
+    schema: list[Column] | None = None
+    conn: AsyncConnection | None = None
+    messages: str = ""
+
+    def __post_init__(self):
         super().__init__()
-        self.query: str = ""
-        self.result = GridList()
-        self.conn: AsyncConnection | None = None
 
     async def _on_call(self, msg: str, **kwargs) -> Any:
         match msg:
             case "run":
                 query = kwargs.get("query", "")
-                result = await run_query(query)
-                self.result.replace(result)
+
+                conn = await PostgresCentral().connect()
+                if conn:
+                    try:
+                        async with conn.cursor() as cur:
+                            await cur.execute(cast(LiteralString, query))
+                            result = await cur.fetchall()
+                            schema = cur.description
+                            self.result.replace(result)
+                            self.schema = schema
+                            self.messages = ""
+                    except Error as e:
+                        self.result.clear()
+                        if self.schema:
+                            self.schema.clear()
+                        self.messages = str(e)
+                    finally:
+                        await cur.close()
+                        await conn.close()
--- app/src/views/sql_view.py
+++ app/src/views/sql_view.py
@@ -1,11 +1,15 @@
 from nicegui import ui
+from nicegui.binding import bind_from
 
+from src.tools.excel import export_to_excel
 from src.viewmodels import ViewModel
 from src.views.sql_grid import SQLGrid
 from src.views.view import View
 
 
 class SQLView(View):
+    _messages: str = ""
+
     def __init__(self, vm: ViewModel):
         super().__init__(vm)
 
@@ -28,10 +32,28 @@ class SQLView(View):
                             self.editor = ui.codemirror(language="PostgreSQL") \
                                 .classes("w-full h-full") \
                                 .bind_value(vm, "query")
+                            self.editor.map_key("F5", self._run_query)
+                            self.editor.map_key("Shift-Enter", self._run_selection)
                             # self.editor.run_method()
 
                     with splitter.after:
-                        SQLGrid(vm)
+                        with ui.tabs().props("dense no-caps") as self.tabs:
+                            result_tab = ui.tab("Result").classes("text-sky-800")
+                            messages_tab = ui.tab("Messages").classes("text-sky-800")
+
+                        with ui.tab_panels(self.tabs, value=result_tab, animated=False) \
+                                .classes("w-full h-full"):
+                            with ui.tab_panel(result_tab) \
+                                    .classes("pl-0 pt-0 pb-0 pr-0"):
+                                self.grid = SQLGrid(vm)
+
+                            with ui.tab_panel(messages_tab) \
+                                    .classes("pl-0 pt-0 pb-0 pr-0"):
+                                ui.code(self.vm.get("messages")) \
+                                    .classes("w-full h-full") \
+                                    .bind_content(self.vm, "messages")
+
+        bind_from(self, "_messages", vm, "messages", backward=self._on_messages_changed)
 
     async def get_selection(self) -> str:
         selected_text = await ui.run_javascript(f'''
@@ -45,7 +67,20 @@ class SQLView(View):
         return selected_text
 
     async def _on_export_to_excel(self):
-        print("Export to Excel")
+        export_to_excel(self.vm.get("result"), "query_result.xlsx")
+
+    async def _run_selection(self):
+        query = await self.get_selection()
+        if query:
+            await self.vm.call("run", query=query)
+        else:
+            ui.notify("No text selected.", color="red")
+
+    def _on_messages_changed(self, messages: str):
+        if messages:
+            self.tabs.set_value("Messages")  # Switch to the Messages tab
+        else:
+            self.tabs.set_value("Result")  # Switch to the Result tab
 
     async def _run_query(self):
         query = await self.get_selection()
